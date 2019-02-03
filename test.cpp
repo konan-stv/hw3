c517aa38f328deb829bd7b6ef9f254e6d93397e1
konan-stv <konan@konanlabs.ru>
2019-02-03 13:55:54 +0300

    yml

--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 
 kChunk *ch;
+void* a[10];
 
 TEST(hwTest, TESTkcCreate) {
   ch = new kChunk(25, 5);
@@ -12,8 +13,30 @@ TEST(hwTest, TESTkcCreate) {
 TEST(hwTest, TESTkcAdd) {
   uint8_t a = ch->getfree();
   if (!a) return;
-  ch->allocate(1);
+  a[0] = ch->allocate(1);
   EXPECT_EQ(ch->getfree(), a-1);
+  a[1] = ch->allocate(1);
+  EXPECT_EQ(ch->getfree(), a-2);
+  a[2] = ch->allocate(1);
+  EXPECT_EQ(ch->getfree(), a-3);
+  a[3] = ch->allocate(1);
+  EXPECT_EQ(ch->getfree(), a-4);
+}
+
+TEST(hwTest, TESTkcShuffle) {
+  uint8_t a = ch->getfree();
+  ch->deallocate(a[2]);
+  EXPECT_EQ(ch->getfree(), a+1);
+  ch->deallocate(a[0]);
+  EXPECT_EQ(ch->getfree(), a+2);
+  a[4] = ch->allocate(1);
+  EXPECT_EQ(ch->getfree(), a+1);
+  a[5] = ch->allocate(1);
+  EXPECT_EQ(ch->getfree(), a);
+  ch->deallocate(a[1]);
+  EXPECT_EQ(ch->getfree(), a+1);
+  a[6] = ch->allocate(1);
+  EXPECT_EQ(ch->getfree(), a);
 }
 
 int main(int argc, char **argv) {
